55f73131fc9d6c1d608c262687e720ba62c5530b
Junkai Hu <junkaihu@JunkaideAir.home>
2019-04-29 23:58:53 -0400

    Added some basic background and animations by Junkai

--- AsteriodGame/GameScene.swift
+++ AsteriodGame/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
@@ -80,9 +80,9 @@ class GameScene: SKScene {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    /*override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    }*/
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
@@ -106,5 +106,87 @@ class GameScene: SKScene {
         }
         
         self.lastUpdateTime = currentTime
+    }
+    
+    var background: SKEmitterNode!
+    var ship: SKSpriteNode!
+    let rocks = ["rock1", "rock2"]
+    var animationSpeed: TimeInterval = 8
+    
+    var timeInterval: TimeInterval = 1
+    var timer: Timer!
+    
+    
+    
+    override func didMove(to view: SKView) {
+        background = SKEmitterNode(fileNamed: "Background")
+        background.position = CGPoint(x: 0, y: self.view!.bounds.height)
+        background.advanceSimulationTime(20)
+        self.addChild(background)
+        
+        background.zPosition = -2
+        
+        ship = SKSpriteNode(imageNamed: "ship")
+        ship.position = CGPoint(x: 0, y: self.frame.size.height / -2.2)
+        self.addChild(ship)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(rockDown), userInfo: nil, repeats: true)
+    }
+    
+    @objc func rockDown() {
+        let rock = SKSpriteNode(imageNamed: rocks[Int.random(in: 0..<2)])
+        let rockWidth = rock.size.width
+        let rockHeight = rock.size.height
+        let rockXRange = Int(self.frame.size.width - rockWidth * 2) / 2
+        let py = self.frame.size.height / 2 + rockHeight
+        let px = CGFloat(Int.random(in: (-rockXRange)...(rockXRange)))
+        //print(rockXRange)
+        rock.position = CGPoint(x: px, y: py)
+        rock.zRotation = CGFloat(Int.random(in: 0..<360))
+        rock.physicsBody = SKPhysicsBody(circleOfRadius: rockWidth)
+        rock.physicsBody?.isDynamic = true
+        rock.physicsBody?.collisionBitMask = 0
+        rock.physicsBody?.contactTestBitMask = 1
+        rock.physicsBody?.categoryBitMask = 2
+        //print("\(px) \(rock) \(self.frame.size.height)")
+        self.addChild(rock)
+        
+        var actions = [SKAction]()
+        
+        actions.append(SKAction.move(to: CGPoint(x: px, y: -py), duration: animationSpeed))
+        actions.append(SKAction.removeFromParent())
+        
+        rock.run(SKAction.sequence(actions))
+        //rock.run(removeRock)
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fire()
+    }
+    
+    func fire() {
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        self.run(SKAction.playSoundFileNamed("Bomb.mp3", waitForCompletion: false))
+        bullet.position = CGPoint(x: ship.position.x, y: ship.position.y + ship.size.height / 2)
+        bullet.physicsBody = SKPhysicsBody(circleOfRadius: bullet.size.width / 2)
+        bullet.physicsBody?.isDynamic = true
+        bullet.physicsBody?.collisionBitMask = 0
+        bullet.physicsBody?.contactTestBitMask = 2
+        bullet.physicsBody?.categoryBitMask = 1
+        bullet.physicsBody?.usesPreciseCollisionDetection = true
+        
+        self.addChild(bullet)
+        
+        var actions = [SKAction]()
+        
+        actions.append(SKAction.move(to: CGPoint(x: bullet.position.x, y: self.frame.size.height / 2 + bullet.size.height), duration: animationSpeed / 20))
+        actions.append(SKAction.removeFromParent())
+        
+        bullet.run(SKAction.sequence(actions))
+        
     }
 }
